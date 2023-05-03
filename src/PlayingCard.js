23d83e2ba532f963ac71fde0e102174f86daaec8
tanakatoo <karmen.tanaka@gmail.com>
2023-05-03 14:10:32 -0400

    step 1 2 done

---
+++ src/PlayingCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+import backOfCard from "./back.png";
+import "./PlayingCard.css"
+import useFlip from "./hook";
+
+/* Renders a single playing card. */
+function PlayingCard({ front, back = backOfCard }) {
+  const [isFacingUp, setIsFacingUp] = useFlip()
+  // const [isFacingUp, setIsFacingUp] = useState(true);
+  // const flipCard = () => {
+  //   setIsFacingUp(isUp => !isUp);
+  // };
+  return (
+    <img
+      src={isFacingUp ? front : back}
+      alt="playing card"
+      onClick={setIsFacingUp}
+      className="PlayingCard Card"
+    />
+  );
+}
+
+export default PlayingCard;
